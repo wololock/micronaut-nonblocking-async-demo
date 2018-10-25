dda1b56ec0e7d03e1640e38be68caadb740c2803
Szymon Stepniak <szymon.stepniak@gmail.com>
2018-10-25 02:36:33 +0200

    product service updated

--- src/main/java/com/github/wololock/micronaut/products/ProductService.java
+++ src/main/java/com/github/wololock/micronaut/products/ProductService.java
@@ -16,23 +16,25 @@ final class ProductService {
 
     private static final Logger log = LoggerFactory.getLogger(ProductService.class);
 
-    private static final Map<String, Supplier<Product>> products = new ConcurrentHashMap<String, Supplier<Product>>() {{
-        put("PROD-001", createProduct("PROD-001", "Micronaut in Action", 29.99, 120));
-        put("PROD-002", createProduct("PROD-002", "Netty in Action", 31.22, 190));
-        put("PROD-003", createProduct("PROD-003", "Effective Java, 3rd edition", 31.22, 600));
-        put("PROD-004", createProduct("PROD-004", "Clean Code", 31.22, 1200));
-    }};
+    private static final Map<String, Supplier<Product>> products = new ConcurrentHashMap<>();
+
+    static {
+        products.put("PROD-001", createProduct("PROD-001", "Micronaut in Action", 29.99, 120));
+        products.put("PROD-002", createProduct("PROD-002", "Netty in Action", 31.22, 190));
+        products.put("PROD-003", createProduct("PROD-003", "Effective Java, 3rd edition", 31.22, 600));
+        products.put("PROD-004", createProduct("PROD-004", "Clean Code", 31.22, 1200));
+    }
 
     public Maybe<Product> findProductById(final String id) {
-        return Maybe.just(id).subscribeOn(Schedulers.io()).map(it -> {
-            log.debug("Retrieving product with id {}", it);
-            return products.getOrDefault(it, () -> null).get();
-        });
+        return Maybe.just(id)
+                .subscribeOn(Schedulers.io())
+                .map(it -> products.getOrDefault(it, () -> null).get());
     }
 
     private static Supplier<Product> createProduct(final String id, final String name, final Double price, final int latency) {
         return () -> {
             simulateLatency(latency);
+            log.debug("Product with id {} ready to return...", id);
             return new Product(id, name, BigDecimal.valueOf(price));
         };
     }
